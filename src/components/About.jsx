0b20d31235bac63508f13b8abb4c920d8b54736f
PavanReddyYk <pavanykreddy24@gmail.com>
2023-10-25 17:55:01 +0530

    about page content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div style={{marginTop : "56px",border:"2px solid red"}}>About</div>
+    <div className='container' style={{ marginTop: "56px" ,color:"white"}}>
+    <h1>About</h1>
+    <h4>A movie web application where users can create accounts and add movies to their watchlist.</h4>
+    </div>
   )
 }
 
